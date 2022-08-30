cbc7dcbd3d1c31b198895365e2b5ec60e43f6baa
Dina <106817687+DinaGezaf@users.noreply.github.com>
2022-08-31 00:18:11 +0200

    beta version with language

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = function(){
-    console.log('Hello Mars!!')
+module.exports = function(lang){
+    if(lang == "es"){
+        console.log('Hola Marte!!')
+    }
+    else{
+        console.log('Hello Mars!!')
+    }
 }
